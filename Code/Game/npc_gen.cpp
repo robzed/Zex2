869ccb7cde21c5166832b7ce7dcd0ae8809e552a
Stu Ball <stuart.ball1@googlemail.com>
2003-09-14 12:09:55 +0000

    Initial issue

---
+++ Code/Game/npc_gen.cpp
@@ -0,0 +1,40 @@
+/*
+ *  npc_gen.c
+ *  zex23xcode
+ *
+ *  Created by Stuart Ball on Sat Sep 13 2003.
+ *  Copyright (c) 2003 Lightsoft. All rights reserved.
+ *
+ */
+
+#ifndef __ZEX_PCH__
+  #include "zselector.h"		// this selects which build we will be doing
+  #include "zplatform.h"
+  #include "LS_Standard.h"
+
+  #if (!defined(__CARBON__)) && ZEX_PROJECT_BUILDER
+    #include <Carbon/Carbon.h>
+  #else
+    #include <fp.h>
+  #endif
+
+  #include "3d_structures_def.h"
+  #include	"zex_misc.h"
+  #include "processor.h"
+  #include "access_ocbs.h"
+  #include "error_handler.h"
+  #include "real_time.h"
+#endif
+
+
+#include "npc_gen.h"
+
+//•••••••••••••••••••••••••••••••••••••••••••••••••••••
+//run_npc_gen looks at the current situation and decides if
+//we should generate an NPC and if so what it should be
+
+void run_npc_gen(void)
+{
+ //if hyper transport is active
+ 
+}
---
+++ Code/Game/npc_gen.h
@@ -0,0 +1,12 @@
+/*
+ *  npc_gen.h
+ *  zex23xcode
+ *
+ *  Created by Stuart ball on Sat Sep 13 2003.
+ *  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <Carbon/Carbon.h>
+
+void run_npc_gen(void);
